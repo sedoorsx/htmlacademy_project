e0f32d1a5cb6fbd30bff13f4609c2008cb1315cf
sedoorsx <a.podoprygolov@gmail.com>
2019-04-01 20:49:04 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -26,11 +26,58 @@ mapClose.addEventListener("click", function (evt) {
     mapModal.classList.remove("modal-show");
 });
 
+//slidos
+
+var sliderButton = document.querySelectorAll('.features-slider .features-slider-button-item');
+var slides = document.querySelectorAll('.features-slider .features-slider-item');
+var currentSlide = 0;
+var sliderNext = document.querySelector('.features-slider-button-forward');
+var sliderPrev = document.querySelector('.features-slider-button-back');
+
+sliderButton.forEach(function callback(element, index) {
+    element.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        sliderButton.forEach(element => {
+            element.classList.remove('active');
+        })
+        element.classList.add('active');
+        slides.forEach(element => {
+            element.classList.remove('active');
+        })
+        slides[index].classList.add('active');
+        currentSlide = index;
+    })
+});
+
+sliderNext.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    if (currentSlide < slides.length - 1) {
+        slides[currentSlide].classList.remove('active');
+        slides[currentSlide + 1].classList.add('active');
+        sliderButton[currentSlide].classList.remove('active');
+        sliderButton[currentSlide + 1].classList.add('active');
+        currentSlide = currentSlide + 1;
+    }
+});
+
+sliderPrev.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    if (currentSlide > 0) {
+        slides[currentSlide].classList.remove('active');
+        slides[currentSlide - 1].classList.add('active');
+        sliderButton[currentSlide].classList.remove('active');
+        sliderButton[currentSlide - 1].classList.add('active');
+        currentSlide = currentSlide - 1;
+    }
+});
+
+//services
+
 var servicesControl = document.querySelectorAll('.services .services-list-item');
 var services = document.querySelectorAll('.services .service');
 
 servicesControl.forEach(function callback(element, index) {
-    element.addEventListener('click', function(evt) {
+    element.addEventListener('click', function (evt) {
         evt.preventDefault();
         servicesControl.forEach(element => {
             element.classList.remove('services-list-item-active');
